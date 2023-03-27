c75455853fee71c19d6a1c08bbb7e78635b1cd2b
Yori112 <awwalyori112@gmail.com>
2023-03-27 10:15:16 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const box1 = document.querySelector(".boxmodel-1");
+const box2 = document.querySelector(".boxmodel-2");
+const submit = document.getElementById("submit");
+const rating = document.getElementById("rating");
+const rates = document.querySelectorAll(".btn");
+
+submit.addEventListener("click", () => {
+  box2.classList.remove("hidden");
+  box1.classList.toggle("hidden");
+});
+
+rates.forEach((rate) => {
+  rate.addEventListener("click", () => {
+    rating.innerHTML = rates.innerHTML;
+  });
+});
